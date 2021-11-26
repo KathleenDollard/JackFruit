9add8a89d48cffe9c00d6c6746e85ae60fa3b4e9
Kathleen Dollard <kdollard@microsoft.com>
2021-11-26 15:52:08 -0800

    WIP

---
+++ Generator.CommonCodeOut/SpaceStringBuilder.fs
@@ -0,0 +1,32 @@
+﻿namespace Generator
+
+open System.Text
+
+
+type SpaceStringBuilder(spacesForIndent: int) =
+    let sb = new StringBuilder()
+    let mutable indentSize = spacesForIndent
+    let mutable indentLevel = 0
+      
+    member _.AppendLine line = 
+        let spaceCount = indentSize * indentLevel
+        let spaces = String.replicate spaceCount " "
+        sb.AppendLine(spaces + line)
+
+    member _.AppendLines lines list = 
+        let spaceCount = indentSize * indentLevel
+        let spaces = String.replicate spaceCount " "
+        for line in lines do
+            sb.AppendLine(spaces + line) |> ignore
+        ()
+        
+    member _.IncreaseIndent =
+        indentLevel <- indentLevel + 1
+
+    member _.DecreaseIndent =
+        if indentLevel > 1 then
+            indentLevel <- indentLevel - 1
+
+    override _.ToString() =
+        sb.ToString()
+
